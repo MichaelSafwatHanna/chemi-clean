9f4bc1b5915b97e61c7d33be5531ea684760e00b
MichaelSafwatHanna <michaelsafwat.hanna@gmail.com>
2020-07-25 14:53:14 +0200

    Add Replication Logic/UI

--- ChemiClean/Controllers/HomeController.cs
+++ ChemiClean/Controllers/HomeController.cs
@@ -1,5 +1,14 @@
-﻿using ChemiClean.Repositories;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ChemiClean.Models;
+using ChemiClean.Repositories;
+using ChemiClean.StorageService;
+using ChemiClean.Util;
 
 namespace ChemiClean.Controllers
 {
@@ -12,9 +21,31 @@ namespace ChemiClean.Controllers
 
         [HttpGet]
         [Route("home/{name}")]
-        public ActionResult ProductByName(string name)
+        public async Task<ActionResult> ProductByName(string name)
         {
-            return View(ProductsRepository.GetInstance().Get(name));
+            var product = ProductsRepository.GetInstance().Get(name);
+            try
+            {
+                var documentBytes = await StorageApi.GetNewProductDocument(product.Url);
+                if (product.LastBackup != "")
+                {
+                    var cachedDocumentBytes = await StorageApi.GetLocalProductDocument(product.Name);
+                    product.IsUpdated = !Util.Util.CompareBytes(documentBytes, cachedDocumentBytes);
+                }
+                product.Url = await StorageApi.PostProductDocument(name, documentBytes);
+                product.LastBackup = DateTime.Now.ToString(CultureInfo.CurrentCulture);
+                ProductsRepository.GetInstance().Update(product.Name);
+            }
+            catch (Exception)
+            {
+                product.IsUrlBroken = true;
+                if (product.LastBackup != "")
+                {
+                    product.Url = StorageApi.ProductsRoute + product.Name;
+                }
+            }
+
+            return View(product);
         }
     }
 }
---
+++ ChemiClean/Util/Util.cs
@@ -0,0 +1,16 @@
+﻿namespace ChemiClean.Util
+{
+    public static class Util
+    {
+        public static bool CompareBytes(byte[] array1, byte[] array2)
+        {
+            if (array1.Length != array2.Length) return false;
+            var i = 0;
+            while (i < array1.Length && (array1[i] == array2[i]))
+            {
+                i++;
+            }
+            return i == array1.Length;
+        }
+    }
+}
